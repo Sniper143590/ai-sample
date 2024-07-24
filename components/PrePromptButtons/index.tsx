c28bd6ca4172d53c2d22462eebcf7831ba423adb
Sniper143590 <snipertiny38@gmail.com>
2024-07-24 22:11:58 +0800

    updated padding of preset buttons

--- components/PrePromptButtons/index.tsx
+++ components/PrePromptButtons/index.tsx
@@ -12,16 +12,16 @@ const PrePromptButtons = ({ handleButtonClick}:PrePromptButtonsProps) => {
     }
     return (
         <div className="flex flex-wrap items-center justify-center mb-3 md:px-5 px-20 w-full"> 
-        {presetButtons && presetButtons.map((item, index) => (
-          <button 
-            key={index} 
-            className="btn-normal mt-1 w-2/5 md:w-full mx-2 border truncate" 
-            onClick={() => handleClick(item)}
-          >
-            {item.text}
-          </button>
-        ))}
-      </div>
+            {presetButtons && presetButtons.map((item, index) => (
+            <button 
+                key={index} 
+                className="btn-normal mt-1 w-2/5 md:w-full mx-2 p-2 border truncate" 
+                onClick={() => handleClick(item)}
+            >
+                {item.text}
+            </button>
+            ))}
+        </div>
     )
 }
 
