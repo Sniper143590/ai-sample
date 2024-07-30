4be7f6bda158cf17093984cbbd9cd7f4ad3d82a1
Sniper143590 <snipertiny38@gmail.com>
2024-07-30 14:10:48 +0800

    updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,13 @@
 'use client'
 
-import AuthProvider from "@/providers/AuthProvider";
 import Dashboard from "@/components/Pages/Dashboradpage";
 import ChatModuleProvider from "@/providers/ChatModuleProvider";
 
 const Chats = () => {
     return (
-        <AuthProvider>
             <ChatModuleProvider>
                 <Dashboard />
             </ChatModuleProvider>
-        </AuthProvider>
     )
 }
 
