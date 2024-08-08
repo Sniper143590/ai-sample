3098506d28c60b66dec504cbaccbb31152cac264
Sniper143590 <snipertiny38@gmail.com>
2024-08-08 16:11:09 +0800

    updated

--- components/Answer/index.tsx
+++ components/Answer/index.tsx
@@ -21,7 +21,7 @@ const Answer = ({ children, loading, time, response, isLast }: AnswerProps) => {
     return (
         <div className={`max-w-[50rem] mb-5`}>
             <div className="flex flex-col">
-                <div className="relative  pt-15 px-6 pb-16 space-y-4 bg-n-2 rounded-[1.25rem] md:p-5 md:pb-14 dark:bg-n-7 h2-style h3-style h4-style h5-style h6-style h1-style">
+                <div className="relative  pt-[1.5rem] px-6 pb-16 space-y-4 bg-n-2 rounded-[1.25rem] md:p-5 md:pb-14 dark:bg-n-7 h2-style h3-style h4-style h5-style h6-style h1-style">
                     {loading ? <Loading /> : (children)} 
                 </div>
                 <div className="w-full">
